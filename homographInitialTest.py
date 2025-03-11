9682d89f5dfa7f0cdb1fa987f9c45c8486fe1661
Isabel Zemke <dzemke@cps.edu>
2025-03-11 10:13:11 -0500

    Making hTest1 return bool actually

--- homographInitialTest.py
+++ homographInitialTest.py
@@ -59,7 +59,7 @@ for i in range(NumChangeLetter):
 # Printing the possibly suspiscious link to visualize it
 
 # function for 1st homograph test (whether the link contains nonlatin characters)
-def HomographTest1(input):
+def HomographTest1(input) -> bool:
     AcceptedCharacters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
                     '`','1','2','3','4','5','6','7','8','9','0','-','=','[',']','\\',';',"'",',','.','/'
@@ -75,8 +75,10 @@ def HomographTest1(input):
     print(input)
     if bad:
         DfAppend(input,'BadLinks.csv')
+        return False
     else:
         DfAppend(input,'SusLinks.csv')
+        return True
 
 def DfAppend(input,csv):
     same=0
